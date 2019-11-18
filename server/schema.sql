bc207852598edfcb02497a6ec37e4e926aed750c
Aizikovich Boris <b.aizikovich@tools4brokers.com>
2019-11-18 16:46:40 +0300

    Add items-table to DB to initial script

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,25 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS items;
 
 CREATE TABLE IF NOT EXISTS users(
-	id INTEGER PRIMARY KEY, 
+	id_user INTEGER PRIMARY KEY, 
 	login TEXT NOT NULL, 
 	credit INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS items(
+	id_item INTEGER PRIMARY KEY, 
+	name TEXT NOT NULL, 
+	price INTEGER
 );
 
 INSERT INTO users(login, credit) VALUES('1',100);
 INSERT INTO users(login, credit) VALUES('2',200);
 INSERT INTO users(login, credit) VALUES('qwe',300);
 INSERT INTO users(login, credit) VALUES('qqq',123);
+
+INSERT INTO items(name, price) VALUES('ship1',123);
+INSERT INTO items(name, price) VALUES('ship2',444);
+INSERT INTO items(name, price) VALUES('ship3',555);
+INSERT INTO items(name, price) VALUES('weapon1',666);
+INSERT INTO items(name, price) VALUES('weapon2',77);
